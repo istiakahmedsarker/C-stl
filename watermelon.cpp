f2f408c4232154232bc071964a7f4c9cf5336a42
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-03 13:56:39 +0600

    solved the watermelon problem

--- watermelon.cpp
+++ watermelon.cpp
@@ -1,7 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    cout << "Watermelon";
+string getEven(int wom){
+    // int halfKilo = wom /2;
+    if(wom % 2 == 0 && wom >2 ){
+        return "Yes";
+    }else{
+        // halfKilo++;
+        // int anotherKilo = wom - halfKilo;
+        return "No";
+    }
+}
+int main()
+{
+        int wom;
+        cin >> wom ;
+
+        string ans = getEven(wom);
+        cout << ans << endl;
+
     return 0;
 }
